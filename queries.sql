ee4ced88273f64314b333e3bfa91e2ff44ca36f7
Vansh Gandhi <vansh.gandhi@gmail.com>
2017-10-09 22:20:45 -0700

    Simplified first query

--- queries.sql
+++ queries.sql
@@ -1,8 +1,8 @@
+-- Names of all the actors in the movie 'Death to Smoochy'
 SELECT CONCAT(a.last, ' ', a.first)
-FROM MovieActor AS ma
-JOIN Movie AS m
-ON ma.mid = m.id
-JOIN Actor AS a
-ON ma.aid = a.id
+FROM MovieActor AS ma, Movie AS m, Actor AS a
 WHERE m.title='Death to Smoochy'
+AND ma.aid = a.id
+AND ma.mid = m.id
 ORDER BY a.last;
+
