97467ca2b40751526ba83db8afbb3ab62e4b8c17
Vansh Gandhi <vansh.gandhi@gmail.com>
2017-10-11 16:24:30 -0700

    Added foreign keys, added not nulls

--- create.sql
+++ create.sql
@@ -1,43 +1,82 @@
 CREATE TABLE Movie(
-	id int, title char(100), year int, rating char(10), company char(50), PRIMARY KEY(id, title)
+	id int NOT NULL,
+	title char(100) NOT NULL,
+	year int NOT NULL,
+	rating char(10),
+	company char(50),
+	PRIMARY KEY(id),
+	INDEX(title)
 );
 
 CREATE TABLE Actor(
-	id int, last char(20), first char(20), sex char(6), dob date, dod date, PRIMARY KEY(id, first, last)
+	id int NOT NULL,
+	last char(20),
+	first char(20),
+	sex char(6),
+	dob date,
+	dod date,
+	PRIMARY KEY(id),
+	INDEX(first, last)
 );
 
 CREATE TABLE Sales(
-	mid int PRIMARY KEY, ticketsSold int, totalIncome int
+	mid int NOT NULL,
+	ticketsSold int NOT NULL,
+	totalIncome int NOT NULL,
+	FOREIGN KEY(mid) REFERENCES Movie(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE Director(
-	id int PRIMARY KEY, last char(20), first char(20), dob date, dod date
+	id int NOT NULL,
+	last char(20),
+	first char(20),
+	dob date,
+	dod date,
+	PRIMARY KEY(id),
+	INDEX(first, last)
 );
 
 CREATE TABLE MovieGenre(
-	mid int PRIMARY KEY, genre char(20)
+	mid int NOT NULL,
+	genre char(20),
+	FOREIGN KEY(mid) REFERENCES Movie(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE MovieDirector(
-	mid int, did int, PRIMARY KEY(mid, did)
+	mid int NOT NULL,
+	did int NOT NULL, 
+	FOREIGN KEY(mid) REFERENCES Movie(id) ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY(did) REFERENCES Director(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE MovieActor(
-	mid int, aid int, role char(50), PRIMARY KEY(mid, aid)
+	mid int NOT NULL,
+	aid int NOT NULL,
+	role char(50),
+	FOREIGN KEY(mid) REFERENCES Movie(id) ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY(aid) REFERENCES Actor(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE MovieRating(
-	mid int PRIMARY KEY, imdb int, rot int
+	mid int NOT NULL,
+	imdb int,
+	rot int,
+	FOREIGN KEY(mid) REFERENCES Movie(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE Review(
-	name char(20), `time` time, mid int, rating int, comment text(500), PRIMARY KEY(name, mid)
+	name char(20),
+	`time` time,
+	mid int NOT NULL,
+	rating int,
+	comment text(500),
+	FOREIGN KEY(mid) REFERENCES Movie(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE MaxPersonID(
-	id int
+	id int NOT NULL
 );
 
 CREATE TABLE MaxMovieID(
-	id int
+	id int NOT NULL
 );
