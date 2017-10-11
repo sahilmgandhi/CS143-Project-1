f889563654b85b2a0d83a4ce634f1c7a31ae97f2
Sahil Gandhi <sahilmgandhi@gmail.com>
2017-10-11 00:11:16 -0700

    Format php website and include table + error handling

--- query.php
+++ query.php
@@ -2,57 +2,84 @@
 <html>
 <body>
 
+<?php $query = "";
+?>
+    <header>
+        <h1>CS 143 Project 1A</h1>
+    </header>
+
+    <h3>By Sahil Gandhi and Vansh Gandhi</h3>
+    <h4>This is the web page for the first part of project 1 for CS 143.</h4>
+    <p>Please enter a SQL query below in order to get started!</p>
+<style>
+    body {
+        background-color: #cedbed
+    }
+</style>
+
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
+    <br><br>
+        <textarea name="query" rows="5" cols="40"> <?php if(isset($_POST['query'])) { 
+                echo htmlentities ($_POST['query']); }?></textarea>
+    <input type="submit" name="submit" value="Submit">  
+</form>
+
 <?php
-$query = "";
-$result = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $db_connection = mysql_connect("localhost", "cs143", "");
+    $db_connection = mysql_connect("localhost", "cs143", "");
   
-  if(!$db_connection) {
-    $errmsg = mysql_error($db_connection);
-    // print "Connection failed: $errmsg" <br/>;
-    exit(1);
-  }
+    if(!$db_connection) {
+        $errmsg = mysql_error($db_connection);
+        print "Connection failed: $errmsg Exiting the program. <br>";
+        exit(1);
+    }
 
-  mysql_select_db("TEST", $db_connection);
-  $query = test_input($_POST["query"]);
-  $sanitized_name = mysql_real_escape_string($name, $db_connection);
-  $query_to_issue = sprintf($query, $sanitized_name);
-  $rs = mysql_query($query_to_issue, $db_connection);
-  $result = "";
-  while($row = mysql_fetch_row($rs)) {
-    // $sid = $row[0];
-    // $name = $row[1];
-    // $email = $row[2];
-    $result .= $row[0].' ';
-    // print "$sid, $name, $email<br/>";
-  }
-  
-  mysql_close($db_connection);
+    $db_selected = mysql_select_db("CS143", $db_connection);
+
+    if (!db_selected){
+    $errmsg = mysql_error($db_selected);
+    print "Unable to select the database specified. Exiting program.";
+    exit(1);
+    }
+
+    $query = processQueryString($_POST["query"]);
+    $sanitized_name = mysql_real_escape_string($name, $db_connection);
+    $query_to_issue = sprintf($query, $sanitized_name);
+    $rs = mysql_query($query_to_issue, $db_connection);
+
+    // Creating the table to show the data
+    echo "<h3>The results from the database are:</h3>\n";
+    echo "<table border=1 cellspacing=1 cellpadding=2>\n";
+    echo "<tr align=center>";
+    for ($i = 0; $i < mysql_num_fields($rs); $i++) {
+        $field = mysql_fetch_field($rs, $i);
+        echo "<th>" . $field->name . "</th>";
+    }
+    echo "</tr>\n";
+    while ($row = mysql_fetch_row($rs)) {
+        echo "<tr align=center>";
+        for ($i = 0; $i < mysql_num_fields($rs); $i++) {
+            $val = $row[$i];
+            if (is_null($val))
+                $val = "N/A";
+            echo "<td>" . htmlspecialchars($val) . "</td>";
+        }
+        echo "</tr>\n";
+    }    
+    echo "</table>\n";
+
+    // Free the result and close the connection to the database
+    mysql_free_result($rs);
+    mysql_close($db_connection);
 }
 
-echo "Welcome to Sahil and Vansh's Project 1A for CS 143. Please enter a SQL query below in order to get started!";
-
-function test_input($data) {
-  $data = trim($data);
-  $data = stripslashes($data);
-  $data = htmlspecialchars($data);
-  return $data;
+function processQueryString($data) {
+    $data = trim($data);
+    return $data;
 }
 
 ?>
-
-<h2>Enter your SQL Query here:</h2>
-<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
-  <br><br>
-  Query: <textarea name="query" rows="5" cols="40"><?php echo $query;?></textarea>
-
-  <input type="submit" name="submit" value="Submit">  
-</form>
-
-<?php print "<br><br>$query<br><br>"; ?>
-<?php print "<br><br>$result<br><br>"; ?>
 
 </body>
 </html>
