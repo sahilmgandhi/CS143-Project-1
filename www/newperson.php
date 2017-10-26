378f0a38622b9e00bf35586eba15d4a04c3de932
Vansh Gandhi <vansh.gandhi@gmail.com>
2017-10-25 18:32:18 -0700

    Create person and display them after submission

--- www/newperson.php
+++ www/newperson.php
@@ -11,42 +11,77 @@
     <input type="text" id="last" name="last" placeholder="Last Name" size="20"> <br> <br>
     <input type="date" id="dob" name="dob" placeholder="Birthday" size="20"> <br> <br>
     <input type="date" id="dod" name="dod" placeholder="Deathday" size="20"> <br> <br>
-    <input type="radio" id="male" name="gender" value="male"> <label for="male">Male</label>
-    <input type="radio" id="female" name="gender" value="female"> <label for="female">Female</label> <br> <br>
-    <input type="submit" name="submit" value="Submit">
+    <input type="radio" id="male" name="sex" value="male"> <label for="male">Male</label>
+    <input type="radio" id="female" name="sex" value="female"> <label for="female">Female</label> <br> <br>
+    <input type="submit" value="Submit">
 </form>
 
 <?php
 
+function validationErrors() {
+    $tablename = $_POST["persontype"];
+    $last = $_POST["last"];
+    $first = $_POST["first"];
+    $sex = $_POST["sex"];
+    $dob = $_POST["dob"];
+    $dod = $_POST["dod"];
+    return (empty($last) || empty($first) || empty($dob) || empty($dod) || empty($tablename) || ($tablename == "actor" && empty($sex)));
+}
+
+function fillOutFields() {
+    // TODO: Fill out fields with previously submitted values
+}
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
+    if (validationErrors()) {
+        fillOutFields();
+        print "Please fill out all fields";
+        exit(1);
+    }
     $db_connection = mysql_connect("localhost", "cs143", "");
-
     if (!$db_connection) {
         $errmsg = mysql_error($db_connection);
         print "Connection failed: $errmsg Exiting the program. <br>";
         exit(1);
     }
-
     $db_selected = mysql_select_db("CS143", $db_connection);
-
     if (!$db_selected) {
         $errmsg = mysql_error($db_selected);
         print "Unable to select the database specified. Exiting program.";
         exit(1);
     }
 
-    $query = trim($_POST["submit"]);
+    $id_update_query = mysql_real_escape_string("UPDATE MaxPersonID SET id=id+1", $db_connection);
+    $id_rs = mysql_query($id_update_query, $db_connection);
+    $id_query = mysql_query("SELECT id FROM MaxPersonID", $db_connection);
+    $id = (int) mysql_fetch_row($id_query)[0];
+    $tablename = ucfirst($_POST["persontype"]); // Capitalize first letter of table name
+    $last = $_POST["last"];
+    $first = $_POST["first"];
+    $sex = $_POST["sex"];
+    $dob = $_POST["dob"];
+    $dod = $_POST["dod"];
+    if ($tablename == "Actor") {
+        $query = "INSERT INTO {$tablename} VALUES ({$id}, '{$last}', '{$first}', '{$sex}', '{$dob}', '{$dod}')";
+    } else {
+        $query = "INSERT INTO {$tablename} VALUES ({$id}, '{$last}', '{$first}', '{$dob}', '{$dod}')"; // No sex
+        // TODO: Don't show sex field if user selects director radio button?
+    }
+    print $query;
     $sanitized_name = mysql_real_escape_string($name, $db_connection);
-    $query_to_issue = sprintf($query, $sanitized_name);
-    $rs = mysql_query($query_to_issue, $db_connection);
+    $sanitized_query = sprintf($query, $sanitized_name);
+    $rs = mysql_query($sanitized_query, $db_connection);
 
-    // TODO: Show results
-
+    // TODO: Check the result of $rs
     // Free the result and close the connection to the database
     mysql_free_result($rs);
     mysql_close($db_connection);
+    header( "Location: person.php?person_type={$tablename}&id={$id}"); // Redirect to display page
+
+
+    // TODO: Redirect to person.php with person id as the parameter, and, with actor or director also as a param
 }
 ?>
+
 </body>
 </html>
--- www/person.php
+++ www/person.php
@@ -1,7 +1,44 @@
+
+<!DOCTYPE html>
+<html>
 <?php
-/**
- * Created by PhpStorm.
- * User: vanshgandhi
- * Date: 10/20/17
- * Time: 11:48 PM
- */
+    $db_connection = mysql_connect("localhost", "cs143", "");
+    if (!$db_connection) {
+        $errmsg = mysql_error($db_connection);
+        print "Connection failed: $errmsg Exiting the program. <br>";
+        exit(1);
+    }
+    $db_selected = mysql_select_db("CS143", $db_connection);
+    if (!$db_selected) {
+        $errmsg = mysql_error($db_selected);
+        print "Unable to select the database specified. Exiting program.";
+        exit(1);
+    }
+    $id = (int) $_GET["id"];
+    $query = "SELECT * FROM {$_GET["person_type"]} WHERE id={$id}";
+    $sanitized_name = mysql_real_escape_string($name, $db_connection);
+    $sanitized_query = sprintf($query, $sanitized_name);
+    $rs = mysql_query($sanitized_query, $db_connection);
+    $actorrow = mysql_fetch_row($rs);
+    $name = $actorrow[2].' '.$actorrow[1];
+    mysql_free_result($rs);
+    // TODO: Check the result of $rs (invalid id or something)
+
+
+    $query = "SELECT * FROM MovieActor AS ma, Movie AS m WHERE ma.aid={$id} AND m.id=ma.mid";
+    $sanitized_name = mysql_real_escape_string($name, $db_connection);
+    $sanitized_query = sprintf($query, $sanitized_name);
+    $moviers = mysql_query($sanitized_query, $db_connection);
+    $movierow = mysql_fetch_row($moviers);
+    // Free the result and close the connection to the database
+    mysql_free_result($moviers);
+    mysql_close($db_connection);
+?>
+<title> <?php echo $name ?> - LMDb </title>
+<body style="background-color:#CEDBED;">
+<h1> <?php echo $name ?> </h1>
+<!--TODO: show actual info from movie result-->
+<?php var_dump($movierow); ?>
+
+</body>
+</html>
