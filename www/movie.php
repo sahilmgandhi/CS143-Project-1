626a393aa59c6f62fd3cdd2e9ec8d5e6061c9dc2
Vansh Gandhi <vansh.gandhi@gmail.com>
2017-10-25 20:54:37 -0700

    Added initial movie code

--- www/movie.php
+++ www/movie.php
@@ -1,7 +1,93 @@
+<!DOCTYPE html>
+<html>
 <?php
-/**
- * Created by PhpStorm.
- * User: vanshgandhi
- * Date: 10/20/17
- * Time: 11:47 PM
- */
+$db_connection = mysql_connect("localhost", "cs143", "");
+if (!$db_connection) {
+    $errmsg = mysql_error($db_connection);
+    print "Connection failed: $errmsg Exiting the program. <br>";
+    exit(1);
+}
+$db_selected = mysql_select_db("CS143", $db_connection);
+if (!$db_selected) {
+    $errmsg = mysql_error($db_selected);
+    print "Unable to select the database specified. Exiting program.";
+    exit(1);
+}
+$id = (int) $_GET["id"];
+$moviequery = "SELECT * FROM Movie WHERE id={$id}";
+$sanitized_name = mysql_real_escape_string($name, $db_connection);
+$sanitized_query = sprintf($moviequery, $sanitized_name);
+$rs = mysql_query($sanitized_query, $db_connection);
+$row = mysql_fetch_row($rs);
+$title = $row[1];
+$year = $row[2];
+$rating = $row[3];
+$company = $row[4];
+mysql_free_result($rs);
+// TODO: Check the result of $rs (invalid id or something)
+
+
+
+$actorquery = "SELECT * FROM MovieActor WHERE mid={$id}";
+$sanitized_name = mysql_real_escape_string($name, $db_connection);
+$sanitized_query = sprintf($actorquery, $sanitized_name);
+$actorrs = mysql_query($sanitized_query, $db_connection);
+
+
+$directorquery = "SELECT * FROM MovieDirector WHERE mid={$id}";
+$sanitized_name = mysql_real_escape_string($name, $db_connection);
+$sanitized_query = sprintf($directorquery, $sanitized_name);
+$directorrs = mysql_query($sanitized_query, $db_connection);
+
+
+$genrequery = "SELECT * FROM MovieGenre WHERE mid={$id}";
+$sanitized_name = mysql_real_escape_string($name, $db_connection);
+$sanitized_query = sprintf($genrequery, $sanitized_name);
+$genrers = mysql_query($sanitized_query, $db_connection);
+
+$reviewsquery = "SELECT * FROM Review WHERE mid={$id}";
+$sanitized_name = mysql_real_escape_string($name, $db_connection);
+$sanitized_query = sprintf($genrequery, $sanitized_name);
+$reviewsrs = mysql_query($sanitized_query, $db_connection);
+
+$reviewsaveragequery = "SELECT AVG(rating) FROM Review WHERE mid={$id}";
+$sanitized_name = mysql_real_escape_string($name, $db_connection);
+$sanitized_query = sprintf($genrequery, $sanitized_name);
+$reviewavg = mysql_fetch_row(mysql_query($sanitized_query, $db_connection))[0];
+
+
+
+// Free the result and close the connection to the database
+mysql_close($db_connection);
+?>
+<title> <?php echo $title ?> - LMDb </title>
+<body style="background-color:#CEDBED;">
+<h1> <?php echo $title ?> </h1>
+<!--TODO: show actual info from movie + actor + director + genre results -->
+
+
+<!--TODO: Show list of directors-->
+<!--TODO: Have a field on this page itself to be able to add directors to this movie-->
+<!--TODO: Show list of actors-->
+<!--TODO: Have a field on this page itself to be able to add actors to this movie-->
+
+
+<!--TODO: Show all other comments for this movie-->
+<br> <br>
+<?php echo "Users gave this movie an average score of {$reviewavg}/5" ?>
+<br> <br>
+<form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+    <input type="text" id="name" name="name" placeholder="Name" size="20"> <br> <br>
+    <select name="rating">
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+    </select> <br>
+    <input type="text" id="comment" name="comment" placeholder="Comment" size="100"> <br> <br>
+    <input type="submit" value="Add Comment">
+</form>
+
+</body>
+</html>
--- www/newmovie.php
+++ www/newmovie.php
@@ -1,7 +1,115 @@
+<!DOCTYPE html>
+<html>
+<title> Create Movie - LMDb </title>
+<body style="background-color:#CEDBED;">
+<h1> Create Movie </h1>
+
+<form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+    <input type="text" id="title" name="title" placeholder="Movie Title" size="20"> <br> <br>
+    <input type="number" id="year" name="year" placeholder="Movie Year" size="4"> <br> <br>
+    <input type="text" id="rating" name="rating" placeholder="Movie Rating" size="10"> <br> <br>
+    <input type="text" id="company" name="company" placeholder="Production Company" size="50"> <br> <br>
+    <!--    TODO: Adding dynamic number of Genres!-->
+    <!--    TODO: Adding dynamic number of Actors!-->
+    <!--    TODO: Adding dynamic number of Directors!-->
+    <input type="submit" value="Submit">
+</form>
+
 <?php
-/**
- * Created by PhpStorm.
- * User: vanshgandhi
- * Date: 10/20/17
- * Time: 11:48 PM
- */
+
+function validationErrors() {
+    $year = $_POST["year"];
+    $title = $_POST["title"];
+    $rating = $_POST["rating"];
+    $company = $_POST["company"];
+    // TODO: Need at least 1 actor
+    // TODO: Need at least 1 director
+    // TODO: Need at least 1 genre
+    return (empty($year) || empty($title) || empty($rating) || empty($company));
+}
+
+function fillOutFields() {
+    // TODO: Fill out fields with previously submitted values
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (validationErrors()) {
+        fillOutFields();
+        print "Please fill out all fields";
+        exit(1);
+    }
+    $db_connection = mysql_connect("localhost", "cs143", "");
+    if (!$db_connection) {
+        $errmsg = mysql_error($db_connection);
+        print "Connection failed: $errmsg Exiting the program. <br>";
+        exit(1);
+    }
+    $db_selected = mysql_select_db("CS143", $db_connection);
+    if (!$db_selected) {
+        $errmsg = mysql_error($db_selected);
+        print "Unable to select the database specified. Exiting program.";
+        exit(1);
+    }
+
+    $id_update_query = mysql_real_escape_string("UPDATE MaxMovieID SET id=id+1", $db_connection);
+    $id_rs = mysql_query($id_update_query, $db_connection);
+    $id_query = mysql_query("SELECT id FROM MaxMovieID", $db_connection);
+    $id = (int) mysql_fetch_row($id_query)[0];
+    $year = $_POST["persontype"];
+    $year = $_POST["year"];
+    $title = $_POST["title"];
+    $rating = $_POST["rating"];
+    $company = $_POST["company"];
+    $query = "INSERT INTO Movie VALUES ({$id}, '{$title}', {$year}, '{$rating}', '{$company}')";
+    $sanitized_name = mysql_real_escape_string($name, $db_connection);
+    $sanitized_query = sprintf($query, $sanitized_name);
+    $rs = mysql_query($sanitized_query, $db_connection);
+
+
+    // TODO: For every Actor entered, add an entry to MovieActor
+    // TODO: For every Director entered, add an entry to MovieDirector
+    // TODO: For every Genre entered, add an entry to MovieGenre
+
+    $actors = [];
+    $directors = []; // todo: store the director id's
+    $genres = []; // todo: store the genre strings
+
+    $query = "INSERT INTO MovieActor VALUES ";
+    foreach ($actors as $actor) {
+        $actorId = ""; // TODO!
+        $role = ""; // TODO!
+        $query .= "({$actorId}, {$id}, '{$role}'),";
+    }
+    $query = substr($query, 0, -1); // omits the last comma
+    $sanitized_name = mysql_real_escape_string($name, $db_connection);
+    $sanitized_query = sprintf($query, $sanitized_name);
+    $rs = mysql_query($sanitized_query, $db_connection);
+
+    $query = "INSERT INTO MovieDirector VALUES ";
+    foreach ($directors as $directorId) {
+        $query .= "({$directorId}, {$id}),";
+    }
+    $query = substr($query, 0, -1); // omits the last comma
+    $sanitized_name = mysql_real_escape_string($name, $db_connection);
+    $sanitized_query = sprintf($query, $sanitized_name);
+    $rs = mysql_query($sanitized_query, $db_connection);
+
+    $query = "INSERT INTO MovieGenre VALUES ";
+    foreach ($genres as $genre) {
+        $query .= "({$id}, '{$genre}'),";
+    }
+    $query = substr($query, 0, -1); // omits the last comma
+    $sanitized_name = mysql_real_escape_string($name, $db_connection);
+    $sanitized_query = sprintf($query, $sanitized_name);
+    $rs = mysql_query($sanitized_query, $db_connection);
+
+
+
+    mysql_free_result($rs);
+    mysql_close($db_connection);
+    header( "Location: movie.php?id={$id}"); // Redirect to display page
+}
+?>
+
+</body>
+</html>
